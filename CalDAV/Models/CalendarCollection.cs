2bc52f5bd4b088f640740a7977dcfa650f4b63c7
Adriano Flechilla <a.flechilla91@gmail.com>
2016-03-09 17:21:40 -0500

    - Added the classes for the EF7.
    	- User.cs to store users data.
    	- CalendarCollection.cs to store the url of the collection for a given user.
    	- CalendarResource.cs to store the man properties of the calendar resources for a given user.

---
+++ CalDAV/Models/CalendarCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalDAV.Models
+{
+    /// <summary>
+    /// To store the url of a user's collection
+    /// </summary>
+    public class CalendarCollection
+    {
+        public int CalendarCollectionId { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ CalDAV/Models/CalendarResource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalDAV.Models
+{
+    /// <summary>
+    /// to store the main properties of a cal resource.
+    /// </summary>
+    public class CalendarResource
+    {
+        [ScaffoldColumn(false)]
+        public int CalendarResourceId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime DtStart { get; set; }
+
+        public DateTime DtEnd { get; set; }
+
+        public string Recurrence { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+
+    }
+}
