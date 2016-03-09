6b56c9a6ac3af282191e55ce4f5fc53b0cc2cc89
Adriano Flechilla <a.flechilla91@gmail.com>
2016-03-09 12:01:42 -0500

    - Added the XMLParser and XMLWriter classes.
    - Added the interfaces ICollectionProperties and ICALDAVProperties for the declaration of the needed properties.

---
+++ CalDAV/XML_Processors/XMLWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalDAV.XML_Processors
+{
+    public class XMLWriter
+    {
+        /// <summary>
+        /// for:
+        /// -calendar description
+        /// -cal timezone
+        /// supported-calendar-component-set
+        /// </summary>
+        const string Namespace = "urn:ietf:params:xml:ns:caldav";
+    }
+}
