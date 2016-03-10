b239f3bf5734d389aca9d78dbb728883b7ab29bc
Adriano Flechilla <a.flechilla91@gmail.com>
2016-03-09 23:12:35 -0500

    - Modified the Context.
    - Added a TODO file.
    - Added some dependencies to the project.json.

--- CalDAV/Models/CALDavContext.cs
+++ CalDAV/Models/CALDavContext.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.Entity;
 
 namespace CalDAV.Models
 {
-    public class CALDavContext
+    public class CalDavContext :DbContext
     {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Caldav;Trusted_Connection=True;");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CalendarCollection>()
+                .HasOne(u=>u.User)
+                .WithMany(cr=>cr.CalendarCollections);
+            modelBuilder.Entity<CalendarResource>()
+                .HasOne(cl => cl.User)
+                .WithMany(u => u.Resources);
+        }
     }
 }
