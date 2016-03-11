f26d2c403336b635ff3d754977c61252b7b458c4
Adriano Flechilla <a.flechilla91@gmail.com>
2016-03-11 15:31:10 -0500

    - Created the method to parse the MKCALENDAR xml.
    - implemented the test for the method XMLMKCalendarParser;

---
+++ CalDAV/Utils/XML_Processors/XMLParsers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CalDAV.Utils.XML_Processors
+{
+    public class XMLParsers
+    {
+        public static Dictionary<string, List<string>> XMLMKCalendarParser(string doc)
+        {
+            var xml = XDocument.Parse(doc);
+            XNamespace ns = "DAV:";
+            var properties = xml.Root.Element(ns + "set").Element(ns + "prop").Elements()
+                .ToDictionary(x => x.Name.LocalName, x =>
+                {
+                    if (x.Descendants().Any())
+                    {
+                        return new List<string>(x.Descendants().Select(y => (string)y.Attribute("name")));
+                    }
+                    else
+                    {
+                        return new List<string>() { (string)x.Value };
+                    }
+                });
+
+            return properties;
+        }
+    }
+}
---
+++ CalDav_tests/XMLTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalDAV.Utils.XML_Processors;
+using Xunit;
+using CalDAV.XML_Processors;
+
+namespace CalDav_tests
+{
+    
+    public class XMLTest
+    {
+        [Fact]
+        public void ParseMKCALENDAR()
+        {
+            var doc = @"<?xml version=""1.0"" encoding=""utf-8""?>
+  <C:mkcalendar xmlns:D=""DAV:"" xmlns:C=""urn:ietf:params:xml:ns:caldav"">
+ <D:set>
+  <D:prop>
+   <D:displayname> Lisa Events</D:displayname>
+      <C:calendar-description xml:lang=""en""> Calendar restricted to events.</C:calendar-description>
+            <C:supported-calendar-component-set>
+             <C:comp name=""VEVENT"" />
+              </C:supported-calendar-component-set>              
+</D:prop>
+</D:set>
+</C:mkcalendar>";
+            var dict =XMLParsers.XMLMKCalendarParser(doc);
+            var expectedDict = new Dictionary<string, List<string>>
+            {
+                {"displayname", new List<string>() {"Lisa Events"}},
+                {"calendar-description", new List<string>() {"Calendar restricted to events."}},
+                {"supported-calendar-component-set", new List<string>() {"VEVENT"}}
+            };
+            Assert.Equal(dict.Count, expectedDict.Count);
+        }
+    }
+}
