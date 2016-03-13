3ac3fead8175cb144ecb9ce8097b245493023e91
Adriano Flechilla <a.flechilla91@gmail.com>
2016-03-13 13:40:43 -0400

    - Added the interface IXMLTreeStructure.
    - Implemented IXMLTreeStructure in XMLTreeStructure. Gonna be use for parsing the building the request and responses of the clients.

---
+++ CalDAV/Utils/XML_Processors/IXMLTreeStructure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalDAV.Utils.XML_Processors
+{
+    public interface IXMLTreeStructure
+    {
+         string NodeName { get;  }
+
+        List<string> Namespaces{get;}
+
+        List<IXMLTreeStructure> Children { get; }
+
+        void AddChild(IXMLTreeStructure child);
+
+        IXMLTreeStructure GetChild(string childName);
+
+        IXMLTreeStructure GetChildAtAnyLevel(string childName);
+    }
+}
---
+++ CalDAV/Utils/XML_Processors/XMLTreeStructure.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalDAV.Utils.XML_Processors
+{
+    /// <summary>
+    /// Tree structure class for parsing and 
+    /// building the xml of the request and responses
+    /// </summary>
+    public class XMLTreeStructure :IXMLTreeStructure
+    {
+        #region Properties
+        /// <summary>
+        /// The name of the XML node.
+        /// </summary>
+        public string NodeName { get; set; }
+
+        /// <summary>
+        /// The namespaces of the node.
+        /// </summary>
+        public List<string> Namespaces { get; }
+
+        /// <summary>
+        /// The children of the node.
+        /// </summary>
+        public List<IXMLTreeStructure> Children { get; set; }
+        #endregion
+
+        public void AddChild(IXMLTreeStructure child)
+        {
+            Children.Add(child);
+        }
+
+        public IXMLTreeStructure GetChild(string childName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IXMLTreeStructure GetChildAtAnyLevel(string childName)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+      
+    }
+}
