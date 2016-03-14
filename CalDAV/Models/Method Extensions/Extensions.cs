b1cc5df6ea5ba8d6c6fb0e8f6216a9830487cd72
Adriano Flechilla <a.flechilla91@gmail.com>
2016-03-14 16:33:23 -0400

    - Added the class Extensions for the extension method to the IEnumerable<VCalendar>.
    - Finish the TimeRangeFilter.
    	- Have to implement other filters for other components beside the VEVENTs

---
+++ CalDAV/Models/Method Extensions/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalDAV.Models.Method_Extensions
+{
+    /// <summary>
+    /// This class contains method extensions for 
+    /// IEnumerable<VCALENDAR>
+    /// </summary>
+    public static class Extensions
+    {
+
+        
+    }
+}
