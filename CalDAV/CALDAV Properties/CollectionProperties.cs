179831eee7fa7b34105f94c03beedb26f9dbfe2d
NachoVazquez <nachovc1410@gmail.com>
2016-04-06 23:52:36 -0400

    CollectionProperties and CollectionResourceProperties:
    *Remove innecesary methods
    *Remove static properties
    *Fix ResolveProperty Method
    *Fix GetAllVisibleProperties Method
    *Fix GetAllPropertyNames Method
    *Added new structure for methods
        -Remove Property
        -Create Property
        -ModifyProperty

--- CalDAV/CALDAV Properties/CollectionProperties.cs
+++ CalDAV/CALDAV Properties/CollectionProperties.cs
@@ -16,93 +16,72 @@ namespace CalDAV.CALDAV_Properties
         public static string CaldavNs => "urn:ietf:params:xml:ns:caldav";
         public static string DavNs => "DAV:";
 
-        /// <summary>
-        /// Contains all the properties that are common for all Calendar Collection.
-        /// </summary>
-        private static Dictionary<string, KeyValuePair<string, string>> XmlGeneralProperties = new Dictionary<string, KeyValuePair<string, string>>()
-        {
-             {"calendar-timezone", new KeyValuePair<string,string>("LaHabana/Cuba", CaldavNs)},
-             {"max-resource-size", new KeyValuePair<string, string>("102400", CaldavNs)},
-             {"min-date-time", new KeyValuePair<string, string>(MinDateTime(), CaldavNs)},
-             {"max-date-time",new KeyValuePair<string, string>( MaxDateTime(), CaldavNs)},
-             {"max-instances", new KeyValuePair<string,string>("10", CaldavNs)},
-             {"getcontentlength", new KeyValuePair<string,string>("0", DavNs)},
-             {"supported-calendar-component-set", new KeyValuePair<string, string>(@"<C:comp name=""VEVENT""/><C:comp name=""VTODO""/>",CaldavNs)},
-             {"supported-calendar-data", new KeyValuePair<string, string>(@"<C:calendar-data content-type=""text/calendar""version=""2.0""/>", CaldavNs) },
-             {"getetag", new KeyValuePair<string, string>("0", DavNs) }
-        };
-
-
-        private static List<string> VisibleGeneralProperties = new List<string>()
-        {
-            "displayname", "resourcetype", "creationdate", "calendar-description"
-        };
-
-        private static string MinDateTime()
-        {
-            var thisMonth = DateTime.Now.Month;
-            var thisDay = DateTime.Now.Day;
-            return
-                new DateTime(DateTime.Now.Year, (thisMonth - 1)==0?12: thisMonth - 1, thisDay>28?28:thisDay).ToUniversalTime()
-                    .ToString("yyyyMMddTHHmmssZ");
-        }
-
-        private static string MaxDateTime()
-        {
-            var thisMonth = DateTime.Now.Month;
-            var thisDay = DateTime.Now.Day;
-            return
-                   new DateTime(DateTime.Now.Year, (thisMonth + 1) == 13?1:thisMonth+1, thisDay > 28 ? 28 : thisDay).ToUniversalTime()
-                       .ToString("yyyyMMddTHHmmssZ");
-        }
 
         /// <summary>
         /// Returns the value of a collection property given its name.
+        /// If error returns the property without value and puts the error in the
+        /// error stack.
         /// </summary>
         /// <param name="collection"></param>
         /// <param name="propertyName"></param>
-        /// <param name="mainNS"></param>
+        /// <param name="mainNs"></param>
+        /// <param name="errorStack">Stores the stack of errors</param>
         /// <returns></returns>
-        public static XmlTreeStructure ResolveProperty(this CalendarCollection collection, string propertyName, string mainNS = "DAV:")
+        public static XmlTreeStructure ResolveProperty(this CalendarCollection collection, string propertyName, string mainNs, Stack<string> errorStack)
         {
-            //First I look to see if is one of the static ones.
-            if (XmlGeneralProperties.ContainsKey(propertyName))
+            ////First I look to see if is one of the static ones.
+            //if (XmlGeneralProperties.ContainsKey(propertyName))
+            //{
+            //    var svalue = XmlGeneralProperties[propertyName];
+            //    var sprop = new XmlTreeStructure(propertyName, svalue.Value);
+            //    sprop.AddValue(svalue.Key);
+            //    return sprop;
+            //}
+            ////TODO: Check all property names
+            //var realPropName = propertyName.ToLower();
+            //string value;
+            //realPropName = realPropName[0].ToString().ToUpper() + realPropName.Substring(1);
+            //realPropName = realPropName.Replace("-", "");
+            //try
+            //{
+            //    value = (string)collection.GetType().GetProperty(realPropName).GetValue(collection);
+            //}
+            //catch (Exception)
+            //{
+            //    value = null;
+            //    //throw new Exception("The value could not be retrieved");
+            //}
+            //XmlTreeStructure prop;
+            //try
+            //{
+            //    if (value != null)
+            //        prop = (XmlTreeStructure)XmlTreeStructure.Parse(value);
+            //    else
+            //        prop = new XmlTreeStructure(propertyName, mainNS);
+            //}
+            //catch (Exception e)
+            //{
+            //    prop = null;
+            //    //throw new Exception("The Property Value Could Not Be Parsed");
+            //    throw e;
+            //}
+            var property = collection.Properties.SingleOrDefault(p => p.Name == propertyName && p.Namespace == mainNs);
+            IXMLTreeStructure prop;
+            if (property != null)
+                prop = property.Value == null
+                    ? new XmlTreeStructure(propertyName, mainNs)
+                    : XmlTreeStructure.Parse(property.Value);
+            else
             {
-                var svalue = XmlGeneralProperties[propertyName];
-                var sprop = new XmlTreeStructure(propertyName, svalue.Value);
-                sprop.AddValue(svalue.Key);
-                return sprop;
+                prop = new XmlTreeStructure(propertyName, mainNs);
             }
-            //TODO: Check all property names
-            var realPropName = propertyName.ToLower();
-            string value;
-            realPropName = realPropName[0].ToString().ToUpper() + realPropName.Substring(1);
-            realPropName = realPropName.Replace("-", "");
-            try
-            {
-                value = (string)collection.GetType().GetProperty(realPropName).GetValue(collection);
-            }
-            catch (Exception)
-            {
-                value = null;
-                //throw new Exception("The value could not be retrieved");
-            }
-            XmlTreeStructure prop;
-            try
-            {
-                if (value != null)
-                    prop = (XmlTreeStructure)XmlTreeStructure.Parse(value);
-                else
-                    prop = new XmlTreeStructure(propertyName, mainNS);
-            }
-            catch (Exception e)
-            {
-                prop = null;
-                //throw new Exception("The Property Value Could Not Be Parsed");
-                throw e;
-            }
+            //var prop = new XmlTreeStructure(propertyName, mainNS);
+            //prop.Value = property?.Value;
+            return (XmlTreeStructure)prop;
 
-            return prop;
+
+
+
         }
 
         /// <summary>
@@ -110,14 +89,22 @@ namespace CalDAV.CALDAV_Properties
         /// an "allprop" property method of Propfind.
         /// </summary>
         /// <param name="collection"></param>
+        /// <param name="errorStack"></param>
         /// <returns></returns>
-        public static List<XmlTreeStructure> GetAllVisibleProperties(this CalendarCollection collection)
+        public static List<XmlTreeStructure> GetAllVisibleProperties(this CalendarCollection collection, Stack<string> errorStack)
         {
             var list = new List<XmlTreeStructure>();
-            foreach (var property in VisibleGeneralProperties)
+            //foreach (var property in VisibleGeneralProperties)
+            //{
+            //    list.Add(ResolveProperty(collection, property));
+            //}
+            foreach (var property in collection.Properties.Where(prop => prop.IsVisible))
             {
-                list.Add(ResolveProperty(collection, property));
+                var tempTree = property.Value == null ? new XmlTreeStructure(property.Name, property.Namespace) : XmlTreeStructure.Parse(property.Value);
+
+                list.Add((XmlTreeStructure)tempTree);
             }
+
             return list;
 
         }
@@ -129,32 +116,67 @@ namespace CalDAV.CALDAV_Properties
         /// <returns></returns>
         public static List<XmlTreeStructure> GetAllPropertyNames(this CalendarCollection collection)
         {
-            var list = new List<XmlTreeStructure>();
+            //foreach (var property in XmlGeneralProperties)
+            //{
+            //    //I took the namespace from the last letter in general properties
+            //    list.Add(new XmlTreeStructure(property.Key, property.Value.Value));
+            //}
 
-            foreach (var property in XmlGeneralProperties)
-            {
-                //I took the namespace from the last letter in general properties
-                list.Add(new XmlTreeStructure(property.Key, property.Value.Value));
-            }
+            ////calendar desription
+            //var description = new XmlTreeStructure("calendar-description", CaldavNs);
+            //list.Add(description);
+            ////a todos los que estan abajo le tienes q pasar el MainNs
+            ////Display Name
+            //var displayName = new XmlTreeStructure("displayname", DavNs);
+            //list.Add(displayName);
 
-            //calendar desription
-            var description = new XmlTreeStructure("calendar-description", CaldavNs);
-            list.Add(description);
-            //a todos los que estan abajo le tienes q pasar el MainNs
-            //Display Name
-            var displayName = new XmlTreeStructure("displayname", DavNs);
-            list.Add(displayName);
+            ////CreationDate
+            //var creationDate = new XmlTreeStructure("creationdate", DavNs);
+            //list.Add(creationDate);
 
-            //CreationDate
-            var creationDate = new XmlTreeStructure("creationdate", DavNs);
-            list.Add(creationDate);
+            ////resource type
+            //var resourceType = new XmlTreeStructure("resourcetype", DavNs);
+            //list.Add(resourceType);
 
-            //resource type
-            var resourceType = new XmlTreeStructure("resourcetype", DavNs);
-            list.Add(resourceType);
-
-            return list;
+            return collection.Properties.Select(property => new XmlTreeStructure(property.Name, property.Namespace)).ToList();
         }
 
+        /// <summary>
+        /// Try to remove the specified property in the collection.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="errorStack"></param>
+        /// <returns></returns>
+        public static bool RemoveProperty(this CalendarCollection collection, string propertyName, Stack<string> errorStack)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Try to create the specified property in the collection.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyValue"></param>
+        /// <param name="errorStack"></param>
+        /// <returns></returns>
+        public static bool CreateProperty(this CalendarCollection collection, string propertyName, string propertyValue,Stack<string> errorStack)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Try to modify the specified property in the collection.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyValue"></param>
+        /// <param name="errorStack"></param>
+        /// <returns></returns>
+        public static bool ModifyProperty(this CalendarCollection collection, string propertyName, string propertyValue, Stack<string> errorStack)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
