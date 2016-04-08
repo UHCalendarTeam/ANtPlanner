69ee90ba6cb1f1c9feb65567ec53ac2ec750e3b2
Adriano Flechilla <a.flechilla91@gmail.com>
2016-04-08 16:51:57 -0400

    - Added the interface for the Report method of the ACL.
    - modified the project.json.

---
+++ ACL/Interfaces/IReportMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TreeForXml;
+
+namespace ACL.Interfaces
+{
+    /// <summary>
+    /// Define the report method available for the resources.
+    /// </summary>
+    public interface IReportMethods
+    {
+        /// <summary>
+        /// Call this method to resolve a REPORT request
+        /// for any of the ACL's defined methods.
+        /// </summary>
+        /// <returns></returns>
+        string ProcessRequest(IXMLTreeStructure body, HttpRequest request);
+
+        /// <summary>
+        /// The DAV:acl-principal-prop-set report returns, for all principals in 
+        /// the DAV:acl property(of the Request-URI) that are identified by 
+        /// http(s) URLs or by a DAV:property principal, the value of the 
+        /// properties specified in the REPORT request body.
+        /// </summary>
+        /// <returns></returns>
+        string AclPrincipalPropSet();
+
+        /// <summary>
+        /// The DAV:principal-match REPORT is used to identify all members (at 
+        /// any depth) of the collection identified by the Request-URI that are 
+        /// principals and that match the current user.
+        /// </summary>
+        /// <returns></returns>
+        string PrincipalMatch();
+
+
+        string PrincipalProperty();
+    }
+}
