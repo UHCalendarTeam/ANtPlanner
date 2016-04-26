c91c4ff13f7ab5f657115fc69c56a563ead11692
Adriano Flechilla <a.flechilla91@gmail.com>
2016-04-26 13:08:36 -0400

    - CalendarQuery Report working as expected.

---
+++ DataLayer/Models/Method Extensions/PropertyExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLayer.Models.Entities;
+using TreeForXml;
+
+namespace DataLayer
+{
+    /// <summary>
+    /// This class contains useful methods
+    /// that extend the behavior of the Property.
+    /// </summary>
+    public static class PropertyExtensionMethods
+    {
+        /// <summary>
+        /// Returns the real value of the given property.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static string PropertyRealValue(this Property property)
+        {
+            return XmlTreeStructure.Parse(property.Value).Value;
+        }
+    }
+}
