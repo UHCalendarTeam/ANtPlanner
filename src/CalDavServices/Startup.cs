72aa3cf397ffba55578bbfa36e4ab110aac1defb
Adriano Flechilla <a.flechilla91@gmail.com>
2016-05-12 11:03:28 -0400

    - Not too much

--- src/CalDavServices/Startup.cs
+++ src/CalDavServices/Startup.cs
@@ -73,9 +73,10 @@ namespace CalDavServices
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. MiddleWares?
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.MinimumLevel = LogLevel.Verbose;
+            loggerFactory.MinimumLevel = LogLevel.Warning;
             loggerFactory.AddConsole();
             loggerFactory.AddDebug();
+           
 
 
             app.UseIISPlatformHandler();
