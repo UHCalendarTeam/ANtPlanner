b247110933bf963b4a7f5b16a4b14c1e7e472ac2
NachoVazquez <nachovc1410@gmail.com>
2016-09-07 14:39:08 -0400

    Minor Changes in project.json

--- src/CalDavServices/Startup.cs
+++ src/CalDavServices/Startup.cs
@@ -121,7 +121,7 @@ namespace CalDavServices
         {
             var host = new WebHostBuilder()
               .UseKestrel()
-              .UseUrls("http://192.168.109.1:5003")
+              .UseUrls("http://10.6.125.119:5003")
               .UseContentRoot(Directory.GetCurrentDirectory())
               .UseIISIntegration()
               .UseStartup<Startup>()
