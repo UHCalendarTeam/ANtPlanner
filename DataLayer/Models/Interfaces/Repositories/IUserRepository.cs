9e805205d446a15bd2e6c192fa96cf73c2215c54
y.arcia <y.arcia9103@gmail.com>
2017-02-01 13:55:46 -0500

    --ending new UserRepository

---
+++ DataLayer/Models/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLayer.Models.Entities.ACL;
+using DataLayer.Models.Entities.Users;
+
+namespace DataLayer.Models.Interfaces.Repositories
+{
+    public interface IUserRepository : IRepository<User, string>
+    {
+        Task<User> FindByEmail(string email);
+
+        Task<User> FindByDisplayName(string name);
+
+        Task<User> FindByPassword(string password);
+
+        Task<User> FindByPrincipalId(string principalId);
+
+        Task<User> FindByPrincipal(Principal principal);
+    }
+}
